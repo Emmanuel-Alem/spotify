66c899df0600ffbecc64c942ecd34a3d931a1410
Emmanuel-Alem <emmaalem012@gmail.com>
2024-11-18 14:56:11 +0300

    first commit

---
+++ lib/presentation/auth/pages/signup.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/widgets/appbar/app_bar.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BasicAppbar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+          width: 40,
+        ),
+      ),
+      body: const Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
